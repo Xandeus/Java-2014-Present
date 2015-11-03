66b60a420a213d6986ef09480aa5ee7a5266593a
Alex McKeever <alexander.mckeever@gmail.com>
2015-11-02 18:16:20 -0600

    Improved fractal generation, implemented basic trees

--- GridPlace/src/Fractals/Shape.java
+++ GridPlace/src/Fractals/Shape.java
@@ -3,7 +3,7 @@ package Fractals;
 import java.awt.Color;
 
 public class Shape {
-	int x,y,r;
+	int x,y,r,dX,dY;
 	boolean isActive = false;
 	Color color = new Color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
 	public Shape(int x, int y, int r){
@@ -11,6 +11,20 @@ public class Shape {
 		this.y = y;
 		this.r = r;
 		
+	}
+	public Shape(int x, int y, int dX,int dY){
+		this.x = x;
+		this.y = y;
+		this.dX = dX;
+		this.dY = dY;
+		
+	}
+	public Shape(int x, int y, int dX,int dY,Color color){
+		this.x = x;
+		this.y = y;
+		this.dX = dX;
+		this.dY = dY;
+		this.color = color;
 	}
 	public int getX(){
 		return x;
@@ -21,6 +35,12 @@ public class Shape {
 	public int getR(){
 		return r;
 	}
+	public int getDX(){
+		return dX;
+	}
+	public int getDY(){
+		return dY;
+	}
 	public void setX(int x){
 		this.x = x;
 	}
@@ -29,6 +49,12 @@ public class Shape {
 	}
 	public void setR(int r){
 		this.r = r;
+	}
+	public void setDX(int dx){
+		dX = dx;
+	}
+	public void setDY(int dy){
+		dY = dy;
 	}
 	public void change(int x, int y, int r){
 		this.x+=x;
@@ -40,6 +66,9 @@ public class Shape {
 	}
 	public Color getColor(){
 		return color;
+	}
+	public void setColor(Color color){
+		this.color = color;
 	}
 	public void changeState(){
 		isActive = !isActive;

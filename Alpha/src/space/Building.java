106027c9a2ee3214c4e8edd5cba30ab7a911a49d
Alex McKeever <alexander.mckeever@gmail.com>
2015-11-22 16:28:51 -0600

    Further improved building construction, tweaked GUI

--- Alpha/src/space/Building.java
+++ Alpha/src/space/Building.java
@@ -3,6 +3,8 @@ package space;
 public interface Building {
 	public String getType();
 	public void function();
+	public int getX();
+	public int getY();
 	//returns place where building resides
 	public CelestialBody getLocation();
 }
--- Alpha/src/space/MiningFacility.java
+++ Alpha/src/space/MiningFacility.java
@@ -2,10 +2,18 @@ package space;
 
 public class MiningFacility implements Building {
 	CelestialBody body;
-	public MiningFacility(CelestialBody body){
+	int posX, posY;
+	public MiningFacility(CelestialBody body, int x, int y){
+		posX = x;
+		posY = y;
 		this.body = body;
 	}
-	
+	public int getX(){
+		return posX;
+	}
+	public int getY(){
+		return posY;
+	}
 	public String getType() {
 		// TODO Auto-generated method stub
 		return "Mining facility";

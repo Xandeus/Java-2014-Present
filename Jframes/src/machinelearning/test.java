f1186817a8706e629430774746690b69adf8cf66
Alex McKeever <alexander.mckeever@gmail.com>
2016-05-17 16:25:22 -0700

    Started work on basic machine learning

---
+++ Jframes/src/machinelearning/test.java
@@ -0,0 +1,11 @@
+package machinelearning;
+
+public class test {
+	public static void main(String[] args){
+		int posX = 0;
+		int posY = 0;
+		int x1 = 3;
+		int y1 = 4;
+		System.out.println((float)(Math.sqrt(((Math.pow((x1-posX), 2)+Math.pow((y1-posY), 2))))));
+	}
+}
